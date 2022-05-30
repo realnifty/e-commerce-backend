d5e34c1efd913bfbf465c45e6fa7c32426137d3f
gordo <rogelioalbertopedraza@gmail.com>
2022-05-29 20:55:02 -0500

    find one product endpoint

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -30,6 +30,32 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [
+      {
+        model: Category,
+        attributes: ['id', 'category_name']
+      },
+      {
+        model: Tag,
+        attributes: ['id', 'tag_name']
+      }
+    ]
+  })
+  .then(dbData => {
+      if (!dbData) {
+        res.status(404).json({ message: 'Product not found!' });
+        return;
+      }
+      res.json(dbData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 // create new product
