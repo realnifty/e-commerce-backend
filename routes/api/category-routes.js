f0472f9d76b0404768256fd712e7858fc142a837
realnifty <rogelioalbertopedraza@gmail.com>
2022-05-29 23:32:48 -0500

    find all categories endpoint

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,6 +6,19 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+      }
+    ]
+  })
+    .then(dbData => res.json(dbData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.get('/:id', (req, res) => {
