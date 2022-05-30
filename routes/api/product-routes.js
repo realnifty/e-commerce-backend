f2c8890857fd1388e71ec5c0cf5c5ec92e94432b
realnifty <rogelioalbertopedraza@gmail.com>
2022-05-29 23:18:05 -0500

    delete endpoint complete

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -138,7 +138,23 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+  // delete one product by its `id` value 
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbData => {
+      if (!dbData) {
+        res.status(404).json({ message: 'Product not found!' });
+        return;
+      }
+      res.json(dbData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
