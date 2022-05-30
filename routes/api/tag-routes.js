c7ea40a9250cad7992b8250c28dd2379e61bb290
realnifty <rogelioalbertopedraza@gmail.com>
2022-05-30 00:32:47 -0500

    find tag by id route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -24,6 +24,28 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+      }
+    ]
+  })
+    .then(dbData => {
+      if (!dbData) {
+        res.status(404).json({ message: 'Tag not found!' });
+        return;
+      }
+      res.json(dbData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ message: 'Tag not found!' });
+    });
 });
 
 router.post('/', (req, res) => {
