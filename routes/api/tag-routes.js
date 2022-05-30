fdd3f7b83fd4beb62dcb989f587d15f3988d177c
realnifty <rogelioalbertopedraza@gmail.com>
2022-05-30 00:47:59 -0500

    tag put/update route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -62,6 +62,21 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbData => {
+      if (!dbData[0]) {
+        res.status(404).json({ message: 'Tag not found!' });
+        return;
+    }
+  })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    })
 });
 
 router.delete('/:id', (req, res) => {
